f402da70bc9e3c61edcff077e3ed3539eac81df5
Edward Hong <edward.hong527@gmail.com>
2021-07-21 19:47:50 +1200

    C07S07 Heroku Config

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-c0gyfa(2&0+5-_7=ec=3^8*b27+#6h3a2$sajzwz_d@e7+-uu^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
