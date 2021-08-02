3aa79c582cc59ebc6d4594275ccbb5876e5a8773
Edward Hong <edward.hong527@gmail.com>
2021-08-02 15:05:34 +1200

    C14S04 LoginRequiredMixin

--- news/articles/views.py
+++ news/articles/views.py
@@ -6,17 +6,17 @@ from django.urls import reverse_lazy
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = (
         "title",
@@ -25,7 +25,7 @@ class ArticleUpdateView(UpdateView):
     template_name = "article_edit.html"
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
